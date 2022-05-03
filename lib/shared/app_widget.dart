17d0c47f710af176180686f407cbb92d051b544e
Mariana Pezzi <mariana.rafaela@senairs.org.br>
2022-05-03 10:28:24 -0300

    Alteração Firebase

--- lib/shared/app_widget.dart
+++ lib/shared/app_widget.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/login/login_page.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
-import 'package:payflow/shared/themes/app_colors.dart';
 
-void main() {
-  runApp(const AppWidget());
-}
+import '../modules/login/login_page.dart';
+import 'themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
